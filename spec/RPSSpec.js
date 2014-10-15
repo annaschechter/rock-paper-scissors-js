01d9a677b5f86628ddda31f397fb5404208dcd71
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-10-15 14:23:26 +0100

    First version of functioning game

--- spec/RPSSpec.js
+++ spec/RPSSpec.js
@@ -1,30 +1,57 @@
 describe("Rock, Paper, Scissors", function() {
+	beforeEach(function() {
+		rock = new Rock;
+		paper = new Paper;
+		scissors = new Scissors;
+	});			
 
 	describe("types of choices", function() {
 
 		it("Rock", function() {
-			rock = new Rock;
 			expect(rock.type).toEqual("rock");
 		});
 
 		it("Paper", function() {
-			paper = new Paper;
 			expect(paper.type).toEqual("paper");
 		});
 
 		it("Scissors", function() {
-			scissors = new Scissors;
 			expect(scissors.type).toEqual("scissors");
 		});
 
 	});
 
 	describe("rules of the game", function() {
-		it("Rock beats scissors", function() {
-			rock = new Rock;
-			scissors = new Scissors;
+		beforeEach(function() {
 			rules = new Rules;
-			expect(rules.play(rock, scissors)).toBe(true);
+		});
+
+		it("rock beats scissors", function() {
+			expect(rules.winner(rock, scissors)).toBe(true);
+		});
+
+		it("scissors don't beat rock", function(){
+			expect(rules.winner(scissors, rock)).toBe(false);
+		});
+
+		it("same choices give draw", function(){
+			expect(rules.winner(rock, rock)).toEqual("draw");
+		});
+
+		it("scissors beat paper", function() {
+			expect(rules.winner(scissors, paper)).toBe(true);
+		});
+
+		it("paper doesn't beat scissors", function() {
+			expect(rules.winner(paper, scissors)).toBe(false);
+		});
+
+		it("paper beats rock", function() {
+			expect(rules.winner(paper, rock)).toBe(true);
+		});
+
+		it("rock doesn't beat paper", function() {
+			expect(rules.winner(rock, paper)).toBe(false);
 		});
 	});
 
--- src/RPSS.js
+++ src/RPSS.js
@@ -12,6 +12,22 @@ function Scissors() {
 
 function Rules(){};
 
-Rules.prototype.play = function(objectOne, objectTwo) {
-	return true
+Rules.prototype.winner = function(objectOne, objectTwo) {
+	if(objectOne.type === objectTwo.type) {return "draw"};
+
+	if(objectOne.type === "rock")
+		if(objectTwo.type === "scissors")
+			return true;
+		else 
+			return false;
+	else if(objectOne.type === "scissors")
+		if(objectTwo.type === "paper")
+			return true;
+		else
+			return false;
+	else
+		if(objectTwo.type === "rock")
+			return true;
+		else
+		return false;
 };
